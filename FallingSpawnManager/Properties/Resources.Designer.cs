8a3c4ef2c530fe59fd547f0b5badeec50341514a
tehtelev <tehtelev@gmail.com>
2026-07-29 19:58:12 +0300

    - Обновлены пакеты и версия нет

--- FallingSpawnManager/Properties/Resources.Designer.cs
+++ FallingSpawnManager/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FallingSpawnManager.Properties {
+namespace Lumos.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FallingSpawnManager.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FallingSpawnManager.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Lumos.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
